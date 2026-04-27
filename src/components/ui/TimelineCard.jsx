0107a1c4ffb26efafbeafa16e419ddf3286535b3
belal-hossainn <belalhossain.rn@gmail.com>
2026-04-27 22:28:08 +0600

    filtering added in timeline

--- src/components/ui/TimelineCard.jsx
+++ src/components/ui/TimelineCard.jsx
@@ -1,11 +1,20 @@
+import { use, useEffect } from "react";
 import { FaCommentDots, FaPhoneAlt, FaVideo } from "react-icons/fa";
 
-const TimelineCard = ({ friend }) => {
+const TimelineCard = ({ friend, sortingType }) => {
     const { name, method, date } = friend;
-  return (
-   <div className="w-full flex gap-4 p-4 bg-base-100 shadow rounded-lg">
 
-            {/* Icon */}
+    useEffect(() => {
+        
+    }, [sortingType]);
+
+     const isVisible = sortingType ? method === sortingType : true;
+
+    if (!isVisible) return null;
+
+  return (
+   <div className="w-full flex gap-4 items-center  p-4 bg-base-100 shadow rounded-lg">
+
             <div className="flex-shrink-0">
                 {method === "text" && (
                     <FaCommentDots className="text-lg" />
@@ -20,19 +29,18 @@ const TimelineCard = ({ friend }) => {
                 )}
             </div>
 
-            {/* Content */}
             <div className="flex flex-col">
                 <h2 className="font-semibold capitalize">
                     {method === "text" && (
-                        <p><span className="font-semibold">Text</span> with {name}</p>
+                        <p className="font-semibold text-[#244D3F]">Text<span className="text-[#64748B] text-sm"> with {name}</span> </p>
                     )}
 
                     {method === "call" && (
-                        <p><span className="font-semibold">Call</span> with {name}</p>
+                       <p className="font-semibold text-[#244D3F]">Call<span className="text-[#64748B] text-sm"> with {name}</span> </p>
                     )}
 
                     {method === "video" && (
-                        <p><span className="font-semibold">Video</span> with {name}</p>
+                        <p className="font-semibold text-[#244D3F]">Video<span className="text-[#64748B] text-sm"> with {name}</span> </p>
                     )}
                 </h2>
 
--- src/pages/timelinePage/TimelinePage.jsx
+++ src/pages/timelinePage/TimelinePage.jsx
@@ -1,18 +1,82 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { FriendContext } from "../../context/FriendProvider";
 import TimelineCard from "../../components/ui/TimelineCard";
 
 const TimelinePage = ({ items }) => {
   const { connectFriends } = useContext(FriendContext);
+  const [isOpen, setIsOpen] = useState(false);
+ const [sortingtype, setSortingType] = useState("");
  
-
-  
   return (
-    <div className="container md:max-w-5xl mx-auto p-4 flex flex-col gap-6">
-      <h1 className="font-bold text-[##244D3F] text-2xl">Timeline</h1>
+    <div className="container md:max-w-5xl mx-auto p-4 pb-10 flex flex-col gap-6">
+      <h1 className="font-bold text-[##244D3F] text-3xl">Timeline</h1>
+      <div>
+       
+<div className="relative inline-block">
+  
+  <button
+    onClick={() => setIsOpen(!isOpen)}
+    className="btn m-1 text-gray-500"
+  >
+    Filter Timeline: {sortingtype} ⬇️
+  </button>
+
+  {isOpen && (
+    <ul className="absolute z-10 mt-2 w-52 bg-base-100 text-gray-500 rounded-box shadow p-2">
+    <li>
+        <button
+          className="w-full text-left p-2 hover:bg-base-200 rounded"
+          onClick={() => {
+            setSortingType("");
+            setIsOpen(false); 
+          }}
+        >
+          All
+        </button>
+      </li>
+      <li>
+        <button
+          className="w-full text-left p-2 hover:bg-base-200 rounded"
+          onClick={() => {
+            setSortingType("call");
+            setIsOpen(false);
+          }}
+        >
+          Call
+        </button>
+      </li>
+
+      <li>
+        <button
+          className="w-full text-left p-2 hover:bg-base-200 rounded"
+          onClick={() => {
+            setSortingType("text");
+            setIsOpen(false);
+          }}
+        >
+          Text
+        </button>
+      </li>
+
+      <li>
+        <button
+          className="w-full text-left p-2 hover:bg-base-200 rounded"
+          onClick={() => {
+            setSortingType("video");
+            setIsOpen(false);
+          }}
+        >
+          Video
+        </button>
+      </li>
+
+    </ul>
+  )}
+</div>
+      </div>
       {
         connectFriends.map((friend) => (
-          <TimelineCard key={friend.id} friend={friend} />
+          <TimelineCard key={friend.id} friend={friend} sortingType={sortingtype} />
         ))
       }
     </div>
